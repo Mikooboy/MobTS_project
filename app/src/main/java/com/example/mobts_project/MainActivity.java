e9f077618af15a81cc145e91103234204b653173
JuliusSau <juliussaukonoja1@gmail.com>
2021-04-23 12:27:38 +0300

    Greetings

--- app/src/main/java/com/example/mobts_project/MainActivity.java
+++ app/src/main/java/com/example/mobts_project/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.mobts_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView helloText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        helloText = findViewById(R.id.Hello);
+        helloText.setText("Heivain");
         //TOIMIIKO?
 
         //Kyllä toimii t:Miko
