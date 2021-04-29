85925d58d6d37bd8ef3487f2578a02e3e386be84
Roope <roope.s.jantunen@gmail.com>
2021-04-29 17:23:15 +0300

    jotain

--- app/src/main/java/com/example/mobts_project/ActivityActivity.java
+++ app/src/main/java/com/example/mobts_project/ActivityActivity.java
@@ -1,28 +1,41 @@
 package com.example.mobts_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 public class ActivityActivity extends AppCompatActivity implements SensorEventListener {
+
     TextView textView;
     SensorManager sensorManager;
     Sensor StepSensor;
     boolean moving;
     int stepCounter = 0;
+    int previousSteps = 0;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity);
-
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            //ask for permission
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION},1 );
+        }
         Intent intent = getIntent();
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -30,6 +43,8 @@ public class ActivityActivity extends AppCompatActivity implements SensorEventLi
         textView.setText("0");
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+
 
         if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!= null){
             StepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
@@ -40,12 +55,13 @@ public class ActivityActivity extends AppCompatActivity implements SensorEventLi
         }
     }
 
+
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         if(event.sensor == StepSensor){
             stepCounter = (int) event.values[0];
             textView.setText(String.valueOf(stepCounter));
-
         }
     }
 
@@ -58,6 +74,7 @@ public class ActivityActivity extends AppCompatActivity implements SensorEventLi
         if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
             sensorManager.registerListener(this, StepSensor, SensorManager.SENSOR_DELAY_UI);
         }
+
     }
 
     @Override
@@ -67,5 +84,7 @@ public class ActivityActivity extends AppCompatActivity implements SensorEventLi
             sensorManager.registerListener(this, StepSensor, SensorManager.SENSOR_DELAY_UI);
         }
     }
+
+
 
 }
